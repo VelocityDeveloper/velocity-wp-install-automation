5329083cb050b7e64b7bc0024889537dc3e0185b
Velocity Developer <velocitydeveloper@users.noreply.github.com>
2026-09-02 09:32:04 +0700

    Make deployment idempotent

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,12 @@ bash -n scripts/website-install-from-manifest scripts/installer-runner
 python3 -m json.tool workflows/website-install-workflow.json >/dev/null
 
 install -d -m 755 "$INSTALL_ROOT/scripts"
-install -m 755 scripts/website-install-from-manifest "$INSTALL_ROOT/scripts/website-install-from-manifest"
-install -m 755 scripts/installer-runner "$INSTALL_ROOT/scripts/installer-runner"
+if [[ "$(realpath scripts/website-install-from-manifest)" != "$(realpath "$INSTALL_ROOT/scripts/website-install-from-manifest")" ]]; then
+  install -m 755 scripts/website-install-from-manifest "$INSTALL_ROOT/scripts/website-install-from-manifest"
+fi
+if [[ "$(realpath scripts/installer-runner)" != "$(realpath "$INSTALL_ROOT/scripts/installer-runner")" ]]; then
+  install -m 755 scripts/installer-runner "$INSTALL_ROOT/scripts/installer-runner"
+fi
 install -m 644 services/installer_status.py /usr/local/bin/installer_status.py
 install -d -m 755 "$WEB_ROOT/installer"
 install -m 644 web/installer/index.html "$WEB_ROOT/installer/index.html"
